fb99847159b5f95345d28111654c67904159db50
gcbw <16908116+gcbw@users.noreply.github.com>
2019-08-26 12:09:40 -0700

    error message when yubikey must be reinserted.
    instead of repeating almost word-for-word the diagnostic error from ykcs11 lib (which is show right above this line btw), show something that have *any* meaning to the end user, who likely do not know what a "reader" is.
    
    Before:
    ```
    $ yubico-piv-tool --verbose -a status
    error: no usable reader found.
    Failed to connect to reader.
    ```
    
    after
    ```
    $ yubico-piv-tool --verbose -a status
    error: no usable reader found.
    Failed to connect to yubikey.
    Have you tried turning it off and on again
    ```
    
    The last line is, obviously, helpful but extraneous, mostly to make a point that i am not a contributor here and someone who knows the end users better than i do should write the actual new text.

--- tool/yubico-piv-tool.c
+++ tool/yubico-piv-tool.c
@@ -1983,7 +1983,7 @@ int main(int argc, char *argv[]) {
   }
 
   if(ykpiv_connect(state, args_info.reader_arg) != YKPIV_OK) {
-    fprintf(stderr, "Failed to connect to reader.\n");
+    fprintf(stderr, "Failed to connect to yubikey.\nHave you tried turning it off and on again?");
     return EXIT_FAILURE;
   }
 
