770bca5e6f36998f24e90eae40e32079e740c5b0
Per Nilsson <per.nilsson@yubico.com>
2019-10-11 15:44:16 +0200

    Increment find_idx even when we hit max returned count

--- ykcs11/ykcs11.c
+++ ykcs11/ykcs11.c
@@ -1447,11 +1447,10 @@ CK_DEFINE_FUNCTION(CK_RV, C_FindObjects)(
   *pulObjectCount = 0;
 
   // Return the next object, if any
-  while(session->find_obj.idx < session->find_obj.n_objects) {
+  while(session->find_obj.idx < session->find_obj.n_objects && *pulObjectCount < ulMaxObjectCount) {
     if(session->find_obj.objects[session->find_obj.idx] != OBJECT_INVALID) {
       *phObject++ = session->find_obj.objects[session->find_obj.idx];
-      if(++(*pulObjectCount) == ulMaxObjectCount)
-        break;
+      (*pulObjectCount)++;
     }
     session->find_obj.idx++;
   }
