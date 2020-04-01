8fb3d594dd2519acb96e0d7e2bc6c8aad5cd795c
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-04-01 09:04:32 +0700

    openssl-compat: check if function exist instead of version number
    LibreSSL is shipping those functions as of LibreSSL 3.0.2,
    Checking for those functions explicitly instead of replying on version
    number.

--- tool/openssl-compat.c
+++ tool/openssl-compat.c
@@ -13,6 +13,7 @@
 #include <string.h>
 #include <openssl/engine.h>
 
+#ifndef HAVE_DECL_RSA_SET0_KEY
 int yubico_RSA_set0_key(RSA *r, BIGNUM *n, BIGNUM *e, BIGNUM *d)
 {
     /* If the fields n and e in r are NULL, the corresponding input
@@ -48,7 +49,9 @@ void yubico_RSA_get0_key(const RSA *r,
     if (d != NULL)
         *d = r->d;
 }
+#endif /* HAVE_DECL_RSA_SET0_KEY */
 
+#ifndef HAVE_DECL_RSA_GET0_FACTORS
 void yubico_RSA_get0_factors(const RSA *r, const BIGNUM **p, const BIGNUM **q)
 {
     if (p != NULL)
@@ -56,7 +59,9 @@ void yubico_RSA_get0_factors(const RSA *r, const BIGNUM **p, const BIGNUM **q)
     if (q != NULL)
         *q = r->q;
 }
+#endif /* HAVE_DECL_RSA_GET0_FACTORS */
 
+#ifndef HAVE_DECL_RSA_GET0_CRT_PARAMS
 void yubico_RSA_get0_crt_params(const RSA *r,
                          const BIGNUM **dmp1, const BIGNUM **dmq1,
                          const BIGNUM **iqmp)
@@ -68,7 +73,9 @@ void yubico_RSA_get0_crt_params(const RSA *r,
     if (iqmp != NULL)
         *iqmp = r->iqmp;
 }
+#endif /* HAVE_DECL_RSA_GET0_CRT_PARAMS */
 
+#ifndef HAVE_DECL_X509_SIG_GETM
 void yubico_X509_SIG_getm(X509_SIG *sig, X509_ALGOR **palg,
                    ASN1_OCTET_STRING **pdigest)
 {
@@ -77,7 +84,9 @@ void yubico_X509_SIG_getm(X509_SIG *sig, X509_ALGOR **palg,
     if (pdigest)
         *pdigest = sig->digest;
 }
+#endif /* HAVE_DECL_X509_SIG_GETM */
 
+#ifndef HAVE_DECL_ECDSA_SIG_SET0
 int yubico_ECDSA_SIG_set0(ECDSA_SIG *sig, BIGNUM *r, BIGNUM *s)
 {
     if (r == NULL || s == NULL)
@@ -95,19 +104,24 @@ void yubico_ECDSA_SIG_get0(const ECDSA_SIG *sig, const BIGNUM **pr, const BIGNUM
     if (ps != NULL)
         *ps = sig->s;
 }
+#endif /* HAVE_DECL_ECDSA_SIG_SET0 */
 
+#ifndef HAVE_DECL_EVP_PKEY_GET0_RSA
 RSA *yubico_EVP_PKEY_get0_RSA(const EVP_PKEY *pkey) {
   if (pkey->type != EVP_PKEY_RSA) {
     return NULL;
   }
   return pkey->pkey.rsa;
 }
+#endif /* HAVE_DECL_EVP_PKEY_GET0_RSA */
 
+#ifndef HAVE_DECL_EVP_PKEY_GET0_EC_KEY
 EC_KEY *yubico_EVP_PKEY_get0_EC_KEY(const EVP_PKEY *pkey) {
   if (pkey->type != EVP_PKEY_EC) {
     return NULL;
   }
   return pkey->pkey.ec;
 }
+#endif /* HAVE_DECL_EVP_PKEY_GET0_EC_KEY */
 
 #endif /* OPENSSL_VERSION_NUMBER || LIBRESSL_VERSION_NUMBER */
--- tool/openssl-compat.h
+++ tool/openssl-compat.h
@@ -22,34 +22,48 @@
 #include <openssl/evp.h>
 #include <openssl/x509.h>
 
+#ifndef HAVE_DECL_RSA_SET0_KEY
 int yubico_RSA_set0_key(RSA *r, BIGNUM *n, BIGNUM *e, BIGNUM *d);
 #define RSA_set0_key yubico_RSA_set0_key
 void yubico_RSA_get0_key(const RSA *r,
                   const BIGNUM **n, const BIGNUM **e, const BIGNUM **d);
 #define RSA_get0_key yubico_RSA_get0_key
+#endif /* HAVE_DECL_RSA_SET0_KEY */
 
+#ifndef HAVE_DECL_RSA_GET0_FACTORS
 void yubico_RSA_get0_factors(const RSA *r, const BIGNUM **p, const BIGNUM **q);
 #define RSA_get0_factors yubico_RSA_get0_factors
+#endif /* HAVE_DECL_RSA_GET0_FACTORS */
 
+#ifndef HAVE_DECL_RSA_GET0_CRT_PARAMS
 void yubico_RSA_get0_crt_params(const RSA *r,
                          const BIGNUM **dmp1, const BIGNUM **dmq1,
                          const BIGNUM **iqmp);
 #define RSA_get0_crt_params yubico_RSA_get0_crt_params
+#endif /* HAVE_DECL_RSA_GET0_CRT_PARAMS */
 
+#ifndef HAVE_DECL_X509_SIG_GETM
 void yubico_X509_SIG_getm(X509_SIG *sig, X509_ALGOR **palg,
                    ASN1_OCTET_STRING **pdigest);
 #define X509_SIG_getm yubico_X509_SIG_getm
+#endif /* HAVE_DECL_X509_SIG_GETM */
 
+#ifndef HAVE_DECL_ECDSA_SIG_SET0
 int yubico_ECDSA_SIG_set0(ECDSA_SIG *sig, BIGNUM *r, BIGNUM *s);
 #define ECDSA_SIG_set0 yubico_ECDSA_SIG_set0
 void yubico_ECDSA_SIG_get0(const ECDSA_SIG *sig, const BIGNUM **pr, const BIGNUM **ps);
 #define ECDSA_SIG_get0 yubico_ECDSA_SIG_get0
+#endif /* HAVE_DECL_ECDSA_SIG_SET0 */
 
+#ifndef HAVE_DECL_EVP_PKEY_GET0_RSA
 RSA *yubico_EVP_PKEY_get0_RSA(const EVP_PKEY *pkey);
 #define EVP_PKEY_get0_RSA yubico_EVP_PKEY_get0_RSA
+#endif /* HAVE_DECL_EVP_PKEY_GET0_RSA */
 
+#ifndef HAVE_DECL_EVP_PKEY_GET0_EC_KEY
 EC_KEY *yubico_EVP_PKEY_get0_EC_KEY(const EVP_PKEY *pkey);
 #define EVP_PKEY_get0_EC_KEY yubico_EVP_PKEY_get0_EC_KEY
+#endif /* HAVE_DECL_EVP_PKEY_GET0_EC_KEY */
 
 #endif /* _WINDOWS */
 #endif /* OPENSSL_VERSION_NUMBER || LIBRESSL_VERSION_NUMBER */
