431f8ee4ea9fc4be2bab0103abf0715dc93d35fa
Aveen Ismail <aveen.ismail@yubico.com>
2020-05-15 09:53:18 +0200

    PIV-102: Edited github action scripts

--- make_src_dist.sh
+++ make_src_dist.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Script to produce an OS X installer .pkg
-# This script has to be run from the source directory
+# Script to produce the source distribution package
 
 VERSION=$1 # Full yubico-piv-tool version, tex 2.1.0
 
@@ -11,10 +10,11 @@ cd ..
 rm -r dist_build
 
 set +e
+
 tar --exclude README.adoc --exclude .git --exclude .github --exclude .gitignore --transform="s/^\./yubico-piv-tool-$VERSION/" -czf yubico-piv-tool-$VERSION.tar.gz .
 exitcode=$?
-echo exitcode=$exitcode
 if [ "$exitcode" != "1" ] && [ "$exitcode" != "0" ]; then
     exit $exitcode
 fi
+
 set -e
