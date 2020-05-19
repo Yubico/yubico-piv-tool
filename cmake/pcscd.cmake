29b5b0fee9f0fa5641e51df8e29f70b07626f79f
Aveen Ismail <aveen.ismail@yubico.com>
2020-05-19 19:00:27 +0200

    PIV-102: Fixed pcsc backend option

--- cmake/pcscd.cmake
+++ cmake/pcscd.cmake
@@ -38,15 +38,10 @@ macro (find_pcscd)
 
     if(${BACKEND} STREQUAL ${BACKEND_ARG_WIN})
         message("Checking for winscard with Windows linkage")
-        find_file(WINSCARD_H_FOUND winscard.h)
-        if(WINSCARD_H_FOUND)
-            set(PCSC_WIN_LIBS "winscard.lib")
-            set(PCSC_LIBRARIES ${PCSC_WIN_LIBS})
-            message("WINSCARD_H_FOUND: ${WINSCARD_H_FOUND}")
-            message("PCSC_WIN_LIBS: ${PCSC_WIN_LIBS}")
-        else(WINSCARD_H_FOUND)
-            message(FATAL_ERROR "cannot find Windows PCSC library/headers")
-        endif(WINSCARD_H_FOUND)
+        set(PCSC_WIN_LIBS "winscard.lib")
+        set(PCSC_LIBRARIES ${PCSC_WIN_LIBS})
+        message("WINSCARD_H_FOUND: ${WINSCARD_H_FOUND}")
+        message("PCSC_WIN_LIBS: ${PCSC_WIN_LIBS}")
     endif(${BACKEND} STREQUAL ${BACKEND_ARG_WIN})
 
     if(${BACKEND} STREQUAL ${BACKEND_ARG_PCSC})
@@ -67,74 +62,7 @@ macro (find_pcscd)
             message (FATAL_ERROR "pcscd not found. Aborting...")
         endif(PCSC_FOUND)
     endif()
-
-
-
-
-
-
-
-
-
-
-#[[
-    if(${BACKEND} STREQUAL ${BACKEND_ARG_CHECK} OR ${BACKEND} STREQUAL ${BACKEND_ARG_PCSC})
-            pkg_check_modules(PCSC REQUIRED libpcsclite)
-            if(PCSC_FOUND)
-                if(VERBOSE_CMAKE)
-                    message("PCSC_FOUND: ${PCSC_FOUND}")
-                    message("PCSC_LIBRARY_DIRS: ${PCSC_LIBRARY_DIRS}")
-                    message("PCSC_LDFLAGS: ${PCSC_LDFLAGS}")
-                    message("PCSC_LDFLAGS_OTHER: ${PCSC_LDFLAGS_OTHER}")
-                    message("PCSC_INCLUDE_DIRS: ${PCSC_INCLUDE_DIRS}")
-                    message("PCSC_CFLAGS_OTHER: ${PCSC_CFLAGS_OTHER}")
-                    message("PCSC_VERSION: ${PCSC_VERSION}")
-                    message("PCSC_INCLUDEDIR: ${PCSC_INCLUDEDIR}")
-                    message("PCSC_LIBDIR: ${PCSC_LIBDIR}")
-                endif(VERBOSE_CMAKE)
-            else(PCSC_FOUND)
-                message (FATAL_ERROR "pcscd not found. Aborting...")
-            endif(PCSC_FOUND)
-
-            set(BACKEND ${BACKEND_ARG_PCSC})
-    endif()
-
-    if(${BACKEND} STREQUAL ${BACKEND_ARG_CHECK})
-        find_file(PCSC_WINSCARD_H_FOUND PCSC/winscard.h)
-        find_file(WINSCARD_H_FOUND winscard.h)
-        if(PCSC_WINSCARD_H_FOUND)
-            set(BACKEND ${BACKEND_ARG_MAC})
-            set(HAVE_PCSC_WINSCARD_H ON)
-            message("Using ${BACKEND_ARG_MAC}")
-        elseif(WINSCARD_H_FOUND)
-            set(BACKEND ${BACKEND_ARG_WIN})
-            message("Using ${BACKEND_ARG_WIN}")
-        endif()
-    endif(${BACKEND} STREQUAL ${BACKEND_ARG_CHECK})
-    message("BACKEND: ${BACKEND}")
-
-    if(${BACKEND} STREQUAL ${BACKEND_ARG_WIN})
-        message("Checking for winscard with Windows linkage")
-        find_file(WINSCARD_H_FOUND winscard.h)
-        set(PCSC_WIN_LIBS "winscard.lib")
-        set(PCSC_LIBRARIES ${PCSC_WIN_LIBS})
-        message("WINSCARD_H_FOUND: ${WINSCARD_H_FOUND}")
-        message("PCSC_WIN_LIBS: ${PCSC_WIN_LIBS}")
-    endif(${BACKEND} STREQUAL ${BACKEND_ARG_WIN})
-
-    if(${BACKEND} STREQUAL ${BACKEND_ARG_MAC})
-        message("Checking for PCSC with Mac linkage")
-        find_file(PCSC_WINSCARD_H_FOUND PCSC/winscard.h)
-        if(PCSC_WINSCARD_H_FOUND)
-            set(HAVE_PCSC_WINSCARD_H ON)
-        endif()
-        set(PCSC_MACOSX_LIBS "-Wl,-framework -Wl,PCSC")
-        set(PCSC_LIBRARIES ${PCSC_MACOSX_LIBS})
-        message("PCSC_WINSCARD_H_FOUND: ${PCSC_WINSCARD_H_FOUND}")
-        message("HAVE_PCSC_WINSCARD_H: ${HAVE_PCSC_WINSCARD_H}")
-        message("PCSC_MACOSX_LIBS: ${PCSC_MACOSX_LIBS}")
-    endif(${BACKEND} STREQUAL ${BACKEND_ARG_MAC})
-]]
+    
     if(${PCSC_LIB} NOT STREQUAL "")
         message("Checking for PCSC with custom lib")
         find_file(PCSC_WINSCARD_H_FOUND PCSC/winscard.h)
