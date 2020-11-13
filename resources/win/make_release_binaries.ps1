038099e5ca0d383d2d5346037990e182e01490bd
Aveen Ismail <aveen.ismail@yubico.com>
2020-11-13 12:42:37 +0100

    PIV-129: Update release script for Windows

--- resources/win/make_release_binaries.ps1
+++ resources/win/make_release_binaries.ps1
@@ -27,7 +27,9 @@ cd $VCPKG_PATH
 .\vcpkg.exe install openssl:$ARCH-windows
 .\vcpkg.exe install getopt:$ARCH-windows
 
-$env:OPENSSL_ROOT_DIR ="$VCPKG_PATH/packages/openssl-windows_$ARCH-windows"
+#$env:OPENSSL_ROOT_DIR ="$VCPKG_PATH/packages/openssl-windows_$ARCH-windows"
+$env:Path ="$VCPKG_PATH/packages/openssl-windows_$ARCH-windows;$env:Path"
+$env:include ="$VCPKG_PATH/packages/openssl-windows_$ARCH-windows/include;$env:include"
 
 # Build for x86 architecture
 cd $SOURCE_DIR
