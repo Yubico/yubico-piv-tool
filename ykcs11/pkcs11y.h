df50a9740234f84879c94c953f1f36012ab400d9
Per Nilsson <per.nilsson@yubico.com>
2020-11-17 17:36:12 +0100

    Using same pkcs11.h as yubihsm-shell

--- ykcs11/pkcs11y.h
+++ ykcs11/pkcs11y.h
@@ -33,8 +33,6 @@
 
 #include "pkcs11.h"
 
-typedef CK_FLAGS * CK_FLAGS_PTR;
-
 // YUBICO specific attributes
 #define CKA_TOUCH_PIN_DEFAULT 0x00000000U
 #define CKA_TOUCH_ALWAYS      0x00000001U
@@ -42,32 +40,5 @@ typedef CK_FLAGS * CK_FLAGS_PTR;
 #define CKA_PIN_ALWAYS        0x00000004U
 #define CKA_PIN_NEVER         0x00000008U
 #define CKA_TOUCH_NEVER       0x00000016U
-
-// Standard stuff that we use but is not in pkcs11.h
-
-#define CKG_MGF1_SHA1			  (0x1UL)
-#define CKG_MGF1_SHA256			(0x2UL)
-#define CKG_MGF1_SHA384			(0x3UL)
-#define CKG_MGF1_SHA512			(0x4UL)
-#define CKG_MGF1_SHA224			(0x5UL)
-
-#define CKZ_DATA_SPECIFIED  (0x1UL) // = CK_BYTE. The only supported option for CK_RSA_PKCS_OAEP_SOURCE_TYPE
-
-typedef unsigned long CK_RSA_PKCS_MGF_TYPE;
-typedef unsigned long CK_RSA_PKCS_OAEP_SOURCE_TYPE;
-
-typedef struct {
-  CK_MECHANISM_TYPE       hashAlg;
-  CK_RSA_PKCS_MGF_TYPE    mgf;
-  CK_ULONG                sLen;
-} CK_RSA_PKCS_PSS_PARAMS, *CK_RSA_PKCS_PSS_PARAMS_PTR;
-
-typedef struct {
-  CK_MECHANISM_TYPE             hashAlg;
-  CK_RSA_PKCS_MGF_TYPE          mgf;
-  CK_RSA_PKCS_OAEP_SOURCE_TYPE  source;
-  CK_VOID_PTR                   pSourceData;
-  CK_ULONG                      ulSourceDataLen;
-} CK_RSA_PKCS_OAEP_PARAMS, *CK_RSA_PKCS_OAEP_PARAMS_PTR;
 
 #endif
