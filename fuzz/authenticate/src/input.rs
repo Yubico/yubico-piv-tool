be28aea86a1575b3642c8ac9960bcf1e304cbd28
Alexandru Geana <alex.geana@yubico.com>
2022-10-04 09:50:45 +0000

    fuzz: rustfmt

--- fuzz/authenticate/src/input.rs
+++ fuzz/authenticate/src/input.rs
@@ -24,13 +24,16 @@ impl From<&BytesInput> for TestCase {
             tc.state_protocol = u32::from_le_bytes(slice[0..4].try_into().expect("no me gusta"));
 
             tc.out_len = u16::from_le_bytes(slice[4..6].try_into().expect("no me gusta")) as u32;
-            tc.plaintext_len = u16::from_le_bytes(slice[6..8].try_into().expect("no me gusta")) as u32;
+            tc.plaintext_len =
+                u16::from_le_bytes(slice[6..8].try_into().expect("no me gusta")) as u32;
 
             tc.out_len = min(tc.out_len, slice.len() as u32 - 8);
             tc.plaintext_len = min(tc.plaintext_len, slice.len() as u32 - 8 - tc.out_len);
 
             tc.out_data = slice[8..8 + tc.out_len as usize].as_ptr();
-            tc.plaintext = slice[8 + tc.out_len as usize..8 + (tc.out_len+tc.plaintext_len) as usize].as_ptr();
+            tc.plaintext = slice
+                [8 + tc.out_len as usize..8 + (tc.out_len + tc.plaintext_len) as usize]
+                .as_ptr();
         }
 
         tc
