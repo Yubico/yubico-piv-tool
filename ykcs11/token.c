caf97bdb0500579ec68b1963223f271c44fd072b
Aveen Ismail <42414844+aveenismail@users.noreply.github.com>
2025-07-14 15:57:12 +0200

    Add null check before parsing hex value (#552)

--- ykcs11/token.c
+++ ykcs11/token.c
@@ -304,7 +304,7 @@ CK_RV token_login(ykpiv_state *state, CK_USER_TYPE user, CK_UTF8CHAR_PTR pin, CK
   ykpiv_rc res;
   int tries = 0;
 
-  if (pin_len == 0) {
+  if (pin_len == 0 || pin == NULL) {
     res = ykpiv_verify_bio(state, NULL, NULL, &tries, false);
   } else if (pin_len >= YKPIV_MIN_PIN_LEN && pin_len <= YKPIV_MAX_PIN_LEN) {
     char term_pin[YKPIV_MAX_PIN_LEN + 1] = {0};
